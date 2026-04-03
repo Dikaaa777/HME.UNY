c570c09ce82cee4f4d81eb277595dc7563cd4473
Dikaaa777 <andika.pratamaa1477@gmail.com>
2026-04-04 01:58:11 +0700

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// NAVBAR SCROLL
+const navbar = document.getElementById('navbar');
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 50){
+    navbar.style.boxShadow = "0 4px 20px rgba(0,0,0,0.08)";
+  } else {
+    navbar.style.boxShadow = "none";
+  }
+});
+
+// REVEAL ANIMATION
 const reveals = document.querySelectorAll(".reveal");
 
 window.addEventListener("scroll", () => {
