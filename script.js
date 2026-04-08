7f9df1a9428b89112d89c017e9132a259ba86b68
Dikaaa777 <andika.pratamaa1477@gmail.com>
2026-04-09 01:05:43 +0700

    script.js

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ window.addEventListener('scroll', () => {
   }
 });
 
-
 // REVEAL
 const reveals = document.querySelectorAll(".reveal");
 
@@ -25,6 +24,39 @@ function revealOnScroll(){
 }
 
 window.addEventListener("scroll", revealOnScroll);
-
-// FIX: langsung aktif saat load
 window.addEventListener("load", revealOnScroll);
+
+// POPUP
+function closePopup(){
+  document.getElementById("welcomePopup").style.display = "none";
+}
+
+// POPUP ONCE
+window.onload = function(){
+  if(localStorage.getItem("visited")){
+    document.getElementById("welcomePopup").style.display = "none";
+  } else {
+    localStorage.setItem("visited", "true");
+  }
+}
+
+// SLIDER DUMMY
+const slider = document.getElementById("slider");
+
+const data = [
+  {judul:"Workshop AI", isi:"Belajar AI dasar"},
+  {judul:"Pelatihan PLC", isi:"Skill industri"},
+  {judul:"Pengabdian", isi:"Teknologi untuk desa"}
+];
+
+data.forEach(item => {
+  const div = document.createElement("div");
+  div.classList.add("slide");
+
+  div.innerHTML = `
+    <h3>${item.judul}</h3>
+    <p>${item.isi}</p>
+  `;
+
+  slider.appendChild(div);
+});
